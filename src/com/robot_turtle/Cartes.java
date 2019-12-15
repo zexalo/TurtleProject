1f82724eb2c3df23ffd9ac11d897fdff8d9b93cd
zexalo <zexal99@icloud.com>
2019-12-15 11:05:47 +0100

    commit intellij

---
+++ src/com/robot_turtle/Cartes.java
@@ -0,0 +1,32 @@
+package com.robot_turtle;
+
+
+public class Cartes extends Board {
+    private String couleur;
+    private String obstacles;
+
+    public Cartes(){
+
+    }
+
+    public Cartes(String couleur, String obstacles){
+        this.couleur=couleur;
+        this.couleur=obstacles;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public String getObstacles() {
+        return obstacles;
+    }
+
+    public void setObstacles(String obstacles) {
+        this.obstacles = obstacles;
+    }
+}
