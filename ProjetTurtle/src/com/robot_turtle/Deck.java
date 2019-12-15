5f2b2e3d87f7bb4a5f9a9c0760d5adc8508336a6
zexalo <zexal99@icloud.com>
2019-12-15 10:57:28 +0100

    Java Turtle constructeur,class, V 1.1

--- ProjetTurtle/src/com/robot_turtle/Deck.java
+++ ProjetTurtle/src/com/robot_turtle/Deck.java
@@ -1,4 +1,31 @@
 package com.robot_turtle;
 
-public class Deck {
+public class Deck extends Board{
+    private int nbrCarte = 37;
+    private int nbrDeck=nbrJoueur;
+
+    public Deck(){
+
+    }
+
+    public Deck(int nbrCarte,int nbrDeck){
+        this.nbrCarte=nbrCarte;
+        this.nbrDeck=nbrDeck;
+    }
+
+    public int getNbrCarte() {
+        return nbrCarte;
+    }
+
+    public void setNbrCarte(int nbrCarte) {
+        this.nbrCarte = nbrCarte;
+    }
+
+    public int getNbrDeck() {
+        return nbrDeck;
+    }
+
+    public void setNbrDeck(int nbrDeck) {
+        this.nbrDeck = nbrDeck;
+    }
 }
--- ProjetTurtle/src/com/robot_turtle/Joyaux.java
+++ ProjetTurtle/src/com/robot_turtle/Joyaux.java
@@ -1,4 +1,41 @@
 package com.robot_turtle;
 
 public class Joyaux {
+    private String couleur;
+    private int posX;
+    private int posY;
+
+    public Joyaux(){
+
+    }
+
+    public Joyaux(String couleur,int posX,int posY){
+        this.couleur=couleur;
+        this.posX=posX;
+        this.posY=posY;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
 }
